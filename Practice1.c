13fe2d1ae434470b472ab08a9e4127f6dd8a0261
nikhitharao <p.raonikhita123@gmail.com>
2021-02-09 21:58:41 +0530

    First version of Practice code

---
+++ Practice1.c
@@ -0,0 +1,15 @@
+/*
+ * Practice1.c
+ *
+ *  Created on: Feb 9, 2021
+ *      Author: Nikhitha P Rao
+ */
+
+#include<stdio.h>
+
+int main()
+{
+	printf("You are a %c %s",'C',"programmer");
+
+	return 0;
+}
